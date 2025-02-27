7fa2307c17a27ce38b6aa21c16faa4e3bc706f49
NachoS <ignaciospaccesii@hotmail.com>
2025-02-27 09:26:06 -0300

    primer commit de CAMJ

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -15,7 +15,7 @@ function Inicio() {
   <div className="absolute inset-0 bg-black opacity-70"></div>
 
   {/* Contenido sobre la imagen */}
-  <div className="relative z-5 flex flex-col h-screen p-[50px] w-[100%] md:w-[60%]  justify-center text-white  font-bold">
+  <div className="relative z-10 flex flex-col h-screen p-[50px] w-[100%] md:w-[60%]  justify-center text-white  font-bold">
     <motion.h1 initial={{ opacity: 0, x: -100 }} whileInView={{ opacity: 1, x:0, transition: {duration: 0.8} }} className='text-3xl md:text-5xl'>Colegio de Abogados Departamento Marcos Juárez</motion.h1>
     <motion.p initial={{ opacity: 0, x: -100 }} whileInView={{ opacity: 1, x:0, transition: {duration: 0.8} }} className='text-1xl md:text-2xl font-light'>Tradición, ética y justicia: 64 años al servicio de los abogados.</motion.p>
   </div>
