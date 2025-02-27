691bd87fccb133b0202a206f42535a948729379c
NachoS <ignaciospaccesii@hotmail.com>
2025-02-27 18:15:39 -0300

    3.1-CAMJ

--- src/components/TramitesMatricula.jsx
+++ src/components/TramitesMatricula.jsx
@@ -39,9 +39,9 @@ function TramitesMatricula() {
                   whileInView={{ opacity: 1, x: 0, transition: { duration: 0.5 } }}
                    className="absolute inset-0 flex mt-[50px] flex-col justify-center">
         <h1 className="text-white text-4xl md:text-5xl xl:text-6xl flex flex-col items-center">Trámites para la <p className='font-bold'>MATRICULACIÓN</p></h1>
-        <h3 className="text-white mt-[25px] mb-[20px] w-[100%] text-center flex flex-col items-center text-md md:text-2xl xl:text-4xl ">información y servicios para colegiarse en el Departamento Marcos Juárez</h3>
+        <h3 className="text-white  mt-[25px] mb-[20px] w-[100%] text-center flex flex-col items-center text-md md:text-2xl xl:text-4xl ">información y servicios para colegiarse en el Departamento Marcos Juárez</h3>
         <div className='flex flex-col items-center'>
-        {enlaces.map((e)=>(<Link className='bg-[#0F0D3C] m-[15px] text-center text-[#f2f2f2] rounded-3xl text-[18px] flex justify-center items-center h-[50px] xl:text-2xl w-[320px] xl:w-[500px] transform scale-100 transition duration-500 hover:scale-110'
+        {enlaces.map((e)=>(<Link className='bg-[#0F0D3C] px-[20px] m-[15px] text-center text-[#f2f2f2] rounded-3xl text-[18px] flex justify-center items-center h-[50px] xl:text-2xl w-[320px] xl:w-[500px] transform scale-100 transition duration-500 hover:scale-110'
             key={e.id}
             to={e.href}
             >{e.link}</Link> ))}
