5347d5602c0ff581e43a74f97642732d9d405eaf
NachoS <ignaciospaccesii@hotmail.com>
2025-03-06 17:02:19 -0300

    3.5 CAMJ

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -29,40 +29,46 @@ function NavBar() {
       link: "Concurso",
     },
   ];
-    return (
-        <div className="z-50 w-[100%] max-w-[1440px] flex justify-between items-center text-black py-6 px-8 md:px-32 bg-[#ffe7d1] drop-shadow-md">
-          <h1 className="text-3xl font-bold">Logo</h1>
-          <div className="hidden xl:flex items-center gap-12 font-semibold text-base">
-            {links.map((l) => (
-              <Link
-                className="p-3 hover:bg-[#15171c] hover:text-[#ffe7d1] rounded-md transition-all "
-                key={l.id}
-                to={l.href}
-              >
-                {l.link}
-              </Link>
-            ))}
-          </div>
-    
-          <IoMdMenu
-            className="xl:hidden block text-5xl cursor-pointer"
-            onClick={() => setIsMenuOpen(!isMenuOpen, console.log(isMenuOpen))}
-          />
-          <div className={`absolute z-5 xl:hidden top-24 left-0 w-full bg-[#ffe7d1] flex flex-col items-center gap-6 font-semibold text-lg transform transition-transform ${isMenuOpen ? "opacity-100 z-50" : "opacity-0"}`}
-            style={{transition: "transform 0.3s ease, opacity 0.3s ease"}}
+  return (
+    <div className="z-50 w-[100%] fixed top-0 max-w-[1440px] flex justify-between items-center text-black py-6 px-8 md:px-32 bg-[#ffe7d1] drop-shadow-md">
+      <h1 className="text-3xl font-bold">Logo</h1>
+
+      {/* Menú Desktop */}
+      <div className="hidden xl:flex items-center gap-12 font-semibold text-base">
+        {links.map((l) => (
+          <Link
+            className="p-3 hover:bg-[#15171c] hover:text-[#ffe7d1] rounded-md transition-all"
+            key={l.id}
+            to={l.href}
           >
-             {links.map((l) => (
-              <Link
-                className="w-full text-center p-4 hover:bg-white transition-all cursor-pointer"
-                key={l.id}
-                to={l.href}
-              >
-                {l.link}
-              </Link>
-            ))}
-          </div>
-        </div>
-      );
-}
+            {l.link}
+          </Link>
+        ))}
+      </div>
+
+      {/* Botón Menú Móvil */}
+      <IoMdMenu
+        className="xl:hidden block text-5xl cursor-pointer"
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+      />
+
+      {/* Menú Móvil */}
+      <div
+        className={`absolute top-24 left-0 w-full bg-[#ffe7d1] flex flex-col items-center gap-6 font-semibold text-lg transition-transform z-50 
+        ${isMenuOpen ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-full hidden"}`}
+      >
+        {links.map((l) => (
+          <Link
+            className="w-full text-center p-4 hover:bg-white transition-all cursor-pointer"
+            key={l.id}
+            to={l.href}
+          >
+            {l.link}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default NavBar
--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -6,25 +6,32 @@ import TramitesMatricula from '../components/TramitesMatricula'
 import DelegacionCorral from '../components/DelegacionCorral'
 function Inicio() {
   return (
-    <>
-   <div className="relative h-screen w-full max-w-[1440px]">
-  {/* Imagen de fondo */}
-  <div  className="absolute inset-0 bg-[url('./assets/FondoInicio.jpg')] bg-cover bg-center bg-no-repeat"></div>
+    <div className="relative h-screen w-full max-w-[1440px]">
+      {/* Imagen de fondo */}
+      <div className="absolute inset-0 bg-[url('./assets/FondoInicio.jpg')] bg-cover bg-center bg-no-repeat pointer-events-none"></div>
 
-  {/* Overlay con opacidad */}
-  <div className="absolute inset-0 bg-black opacity-70"></div>
+      {/* Overlay con opacidad */}
+      <div className="absolute inset-0 bg-black opacity-70 pointer-events-none"></div>
 
-  {/* Contenido sobre la imagen */}
-  <div className="relative z-10 flex flex-col h-screen p-[50px] w-[100%] md:w-[60%]  justify-center text-white  font-bold">
-    <motion.h1 initial={{ opacity: 0, x: -100 }} whileInView={{ opacity: 1, x:0, transition: {duration: 0.8} }} className='text-3xl md:text-5xl'>Colegio de Abogados Departamento Marcos Juárez</motion.h1>
-    <motion.p initial={{ opacity: 0, x: -100 }} whileInView={{ opacity: 1, x:0, transition: {duration: 0.8} }} className='text-1xl md:text-2xl font-light'>Tradición, ética y justicia: 64 años al servicio de los abogados.</motion.p>
-  </div>
-</div>
-<Links></Links>
-<TramitesMatricula></TramitesMatricula>
-<DelegacionCorral/>
-    </>
-  )
-}
+      {/* Contenido sobre la imagen */}
+      <div className="relative z-10 flex flex-col h-screen p-[50px] w-[100%] md:w-[60%] justify-center text-white font-bold">
+        <motion.h1
+          initial={{ opacity: 0, x: -100 }}
+          whileInView={{ opacity: 1, x: 0, transition: { duration: 0.8 } }}
+          className="text-3xl md:text-5xl"
+        >
+          Colegio de Abogados Departamento Marcos Juárez
+        </motion.h1>
+        <motion.p
+          initial={{ opacity: 0, x: -100 }}
+          whileInView={{ opacity: 1, x: 0, transition: { duration: 0.8 } }}
+          className="text-1xl md:text-2xl font-light"
+        >
+          Tradición, ética y justicia: 64 años al servicio de los abogados.
+        </motion.p>
+      </div>
+    </div>
+  );
+};
 
 export default Inicio
