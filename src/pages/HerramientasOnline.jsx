6988ff12cc1fa89bd56e0ba55f87267554122e84
NachoS <ignaciospaccesii@hotmail.com>
2025-03-31 16:42:09 -0300

    1.7 CAMJ

--- src/pages/HerramientasOnline.jsx
+++ src/pages/HerramientasOnline.jsx
@@ -5,16 +5,15 @@ function HerramientasOnline() {
   return (
     <div className="mt-36 mb-16 flex flex-col flex-wrap justify-center items-center">
       <a
-        href="/upload/EstatutoColegioAbogados.doc"
-        download="EstatutoColegioAbogados.doc"
+        href="/upload/EstatutosColegioAbogados.doc" download
         className="px-6 m-2 py-3 text-[15px] xl:text-[18px] w-[300px] xl:w-[40%] text-center border-[#15171c] border-2 text-black font-semibold rounded-md hover:bg-[#15171c] hover:text-white transition duration-300 shadow-md"
       >
         Descargar Estatuto Colegio de Abogados
       </a>
 
       <a
-        href="/upload/EstatutoComisionJovenesAbogados.doc"
-        download="EstatutoComisionJovenesAbogados.doc"
+        href="/upload/Estatutodelacomisióndejovenesabogados.doc"
+        download
         className="px-6 m-2 py-3 text-[15px] xl:text-[18px] w-[300px] xl:w-[40%] text-center border-[#15171c] border-2 text-black font-semibold rounded-md hover:bg-[#15171c] hover:text-white transition duration-300 shadow-md"
       >
         Descargar Estatuto Comisión Jóvenes Abogados
