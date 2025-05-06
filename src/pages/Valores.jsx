0cbbebde8f18378ef27bdcb2c8765a0f13d3d03a
NachoS <ignaciospaccesii@hotmail.com>
2025-05-06 15:50:41 -0300

    2.6 CAMJ

--- src/pages/Valores.jsx
+++ src/pages/Valores.jsx
@@ -6,19 +6,19 @@ function Valores() {
         {
             id:1,
             titulo:"Valor del JUS",
-            fechaAct:"Desde el 01/04/2025",
-            valor:"$32.348,40"
+            fechaAct:"Desde el 06/05/2025",
+            valor:"$32.995,37"
         },
         {
             id:2,
             titulo:"Unidad Económica",
-            fechaAct:"Desde el 01/04/2025",
-            valor:"$8.525.859,38"
+            fechaAct:"Desde el 06/05/2025",
+            valor:"$8.696.376,57"
         },
         {
             id:3,
             titulo:"Aporte colegial",
-            fechaAct:"Desde el 01/04/2025",
+            fechaAct:"Desde el 06/05/2025",
             valor:"$20.500,00"
         },
     ]
